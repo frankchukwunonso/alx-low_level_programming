949cc5c4329280547b4c8f4688885c0ba8adbeb0
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-09 03:14:40 -0800

    putzz

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+i#include <stdio.h>
 /*
  * main - Entry point
  * Return: Success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n")
+	printf("with proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }
