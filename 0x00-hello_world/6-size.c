d5ad3b4b2724ac584a2a1592120173f04ef2bb88
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-09 03:46:31 -0800

    type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Success
+ */
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	return (0);
+}
