b7d6f4c36ab6e39f34de82827f9a93945c39790f
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-10 15:57:42 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Success
+*/
+int main(void)
+{
+	/*initialise ch*/
+	char ch;
+
+	/*print the lowercase alphabets*/
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch\n)
+	}
+
+
+	return (0);
+}
+
+
