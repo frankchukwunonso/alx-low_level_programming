84c7c576d877f254cc5f1d32cdc5a9122735e628
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-10 17:22:32 -0800

    alphabet soup indeed

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: success
+ */
+int main(void)
+{
+	/*initialising the lowercase alpabet*/
+	char ch = 'a';
+
+	/*loop to print lowercase letters except 'q' and 'e'*/
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' and ch != 'e')
+		{
+		putchar(ch);
+		}
+	}
+
+	return (0)
+}
