52464c50f02c1d4185bde3dc8d219392c15640a2
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-11 03:56:10 -0800

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Return: success
+ */
+int main(void)
+{
+	/*initialise digits*/
+	char digits = '0'
+
+	for (digits == 0; digits <= 9; digits++)
+	{
+	putchar(digits)
+	}
+
+	return (0)
+}
