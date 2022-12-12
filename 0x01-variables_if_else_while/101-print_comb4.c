2a103bc9e38d619cbae434a3d4745b5bf8e260a6
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-12 07:57:32 -0800

    3-digits combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - 3- digit combo
+ * Return: success
+ */
+int main(void)
+{
+	int cent;/* declare variables */
+	int tens;
+	int units;
+
+	for (units = '0'; units <= '9'; units++)/* loop for 3-digit numbers */
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (cents = '0'; cents <= '9'; cents++)
+			{
+				if (units != tens != cents || cents > tens > units)
+				{
+					putchar(cents);
+					putchar(tens);
+					putchar(units);
+					if (cent == '7' && tens == '8' && units == '9')
+					{/* conditions for digits combo */
+						break;
+					}
+				}
+			}
+		}
+	}
+	putchar(',');/* new line */
+
+	return (0);
+}
