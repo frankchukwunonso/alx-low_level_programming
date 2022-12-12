3338889d90711363401a6a44048c39c3e8e1ca9c
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-12 06:37:45 -0800

    2-digit combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - 2-digits combination with no repeat or sameness
+ * Return: success
+ */
+int main(void)
+{
+	int units;
+	int tens;
+
+	for (units = '0'; units <= '9'; units++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			if (ten != units && tens < units)
+/**
+ * The first conjunct is to prevent a 2-digit number
+ * with same digits from occuring while the second
+ * conjunct is to restrict the 2-digit-combination to
+ * their first occurence only.
+ */
+			{
+				putchar(units)
+						/* print the 2-digit combinations*/
+				putchar(tens)
+				putchar(',')
+				putchar(' ')
+			}
+		}
+	}
+	putchar('\n')/* print new line */
+
+	return (0)
+}
