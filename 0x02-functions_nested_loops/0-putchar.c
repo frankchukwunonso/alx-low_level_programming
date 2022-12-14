200f656ee021a1be6dccd6541569b0479a30b61e
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-14 15:41:19 -0800

    _putchar print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,14 @@
 
 int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
 
 	return (write(1, &c,1));
 }
