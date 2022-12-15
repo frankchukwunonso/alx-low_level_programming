21346ab9d646b24db0e3b3a094c05a95168d33b1
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-15 11:36:49 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ *
+ * @a: it's a parameter
+ *
+ *Return: d or a
+ */
+int _abs(int a)
+{
+	int d;
+
+	if (a < 0)
+	{
+		d =  a * -1;
+	return (d);
+	}
+	else
+	{
+		return (a);
+	}
+}
