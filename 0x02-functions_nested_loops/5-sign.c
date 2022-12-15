bab388c7d6bbab5d6360c920038e16eb95ea5a82
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-15 09:44:29 -0800

    check the number signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,16 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		return (-1);
+		_putchar('-');
 	}
 	else if (n == 0)
 	{
 		return (0);
+		_putchar('0');
 	}
-	else 
+	else
 	{
 		return (1);
+		_putchar('+');
 	}
 }
