0a2bac605dadd7b4a1ef8bc0010d0f851285e148
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-15 04:46:53 -0800

    alphabetsx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i = 0; i <= 1; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
