36bfb9a74548e953a68dab6cfefaba9257ba0287
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-15 19:14:05 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,17 @@ void times_table(void)
 {
 	int row, col, d;
 
-	for (col = 0; col <= 9; col++)
+	for (col = '0'; col <= '9'; col++)
 	{
-		for (row = 0; row <= 9; row++)
+		for (row = '0'; row <= '9'; row++)
 		{
 			d = row * col;
 			_putchar(d);
-			if (row != 9)
+			if (row != '9')
 			{
 				_putchar(',');
 				_putchar(' ');
-				if ((d % 10) == 0)
+				if ((d % 10) == '0')
 				{
 					_putchar(' ');
 				}
