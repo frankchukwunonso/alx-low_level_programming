52e9fb00691683059d89d2d4f1bb955adb5048b8
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-15 18:32:20 -0800

    mins

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * jack_bauer - print every minutesof the day
+ */
+void jack_bauer(void)
+{
+	int mins1;
+	int hr1;
+
+	for (hr1 = 0; hr1 <= 23; hr1++)
+	{
+		for (mins1 = 0; mins1 <= 59; mins1++)
+		{
+			_putchar((hrs1 / 10) + '0');
+			_putchar((hrs1 % 10) + '0');
+			_putchar(':');
+			_putchar((mins1 / 10) + '0');
+			_putchar((mins1 % 10) + '0');
+		}
+	}
+}
