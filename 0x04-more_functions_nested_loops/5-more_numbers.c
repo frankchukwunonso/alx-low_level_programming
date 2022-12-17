3f8a733f81f539c2ab186678843482fb4610ab39
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-17 09:27:00 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14, except 2 and 4
+ */
+void more_numbers(void);
+{
+	int d, num;
+
+	for (d = 0; d <= 10; d++)
+	{
+		for (num = 0; num < 14 ; num++)
+		{
+			if (!(num == 2 || num == 4))
+			{
+				if ((num / 10) > 0)
+				{
+					_putchar((num / 10) + 48);
+				}
+				_putchar((num % 10) + 48);
+			}
+		}
+		_putchar('\n')
+	}
+}
