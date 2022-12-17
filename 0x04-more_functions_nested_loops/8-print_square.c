1c6f0a9512081c9d4a04f53a6a009b2925e31218
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-17 12:58:08 -0800

    draw square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - it prints square of given size
+ *
+ * @size: it is a parameter
+ */
+void print_square(int size)
+{
+	int row, col;
+
+	if (size > 0)
+	{
+		for (col = 0, col <= (size - 1); col++)
+		{
+			for (row = 0; row <= (size - 1); row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
