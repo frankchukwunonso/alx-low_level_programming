edf88d31b061d90cc0e4f42311153e9a09d90811
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-17 05:51:31 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper: checks for uppercase character
+ *
+ *@c: it is a parameter
+ *
+ * Returns: returns 1 or 0
+ */
+int _isupper(int c)
+{
+	int b;
+
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		if (c == b)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
