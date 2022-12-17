67ba0ddd7788538b20ea0fd7cb5b00cce4cb4892
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-17 10:39:32 -0800

    diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonL lines
+ *
+ * @n: it is a parameter
+ */
+void print_diagonal(int n)
+{
+	int d;
+
+	for (d = 0; d <= (n - 2); d++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
