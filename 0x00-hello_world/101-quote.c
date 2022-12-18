80974279b71a7ba59412c58733c0d12b658f1ba1
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-17 21:34:45 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 /**
  * main - prints quote
  *
- * Return: returns /
+ * Return: returns 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art i"
-			"s useful\" - Dora Korpar, 2015-10-19");
-	putchar('\n');
+	fprintf(stderr, "and that piece of art "
+			"is useful\" - Dora Korpar, 2015-10-19\n")
 
 	return (1);
 }
