a68ec95d25fe8183022ee94d102db86af2eb88ab
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-19 19:28:31 -0800

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints strings in reverse
+ * @s: it is a string
+ *
+ */
+void print_rev(char *s)
+{
+	int index = 0;
+	int len = 0;
+
+	while (s[index] != '\0') /* to determine string length */
+	{
+		len++;
+		index++;
+	}
+	while (len <= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
