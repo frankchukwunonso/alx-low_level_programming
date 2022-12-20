a2e6c7a5f71cce339a2b6fd79515c434b336f5eb
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-20 12:55:54 -0800

    print every other number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,11 @@ void puts2(char *str)
 {
 	int a = 0;
 
-	while (*(str + a) != '\0')
+	while (str[a] != '\0')
 	{
-		*(str + a);
-
 		if (a % 2 == 0)
 		{
-			_putchar((*str + a));
+			_putchar(str[a]);
 		}
 		a++;
 	}
