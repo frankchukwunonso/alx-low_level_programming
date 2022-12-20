dc306ba6eda7ffb32c85069a6150ea87614e22f7
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-19 19:02:57 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts
+ * @str: it is a string parameter
+ */
+void _puts(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
+}
