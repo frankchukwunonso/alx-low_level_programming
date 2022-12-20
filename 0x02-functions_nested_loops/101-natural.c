94c911c00c3267a497d6771370a1311ebf6ea5da
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-20 02:02:05 -0800

    corrected semantic error on incrementing variable

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,7 +8,7 @@ int main(void)
 	int sum_mul = 0;
 	int n = 0;
 
-	while (sum_mul < 1024)
+	while (n < 1024)
 	{
 		n++;
 		if ((n % 3) == 0 || (n % 5) == 0)
@@ -16,6 +16,7 @@ int main(void)
 			sum_mul = sum_mul + n;
 		}
 	}
-	printf("%d", sum_mul);
+	printf("%d\n", sum_mul);
+
 	return (0);
 }
