e70e57bad8b8442f328742736f888a0e0b274502
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-20 20:47:35 -0800

    function to reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,19 @@
  */
 void rev_string(char *s)
 {
-	int 
-	while (*(s + n) != '\0')
-	{
+	int i = 0;
+	int r, l;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	r = i;
+	i = i - 1;
+	for (l = 0; l < r; l++)
+	{
+		s[l] = s[i];
+		i--;
+	}
+}
