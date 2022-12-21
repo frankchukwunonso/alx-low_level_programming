61e518306fd7c9925828886d5135744615542071
frankchukwunonso <frankchukwunonso@gmail.com>
2022-12-20 20:54:05 -0800

    reverse string- added variable

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int r, l;
+	char d;
 
 	while (s[i] != '\0')
 	{
@@ -18,7 +19,8 @@ void rev_string(char *s)
 	i = i - 1;
 	for (l = 0; l < r; l++)
 	{
-		s[l] = s[i];
+		q = s[i];
+		s[l] = q;
 		i--;
 	}
 }
